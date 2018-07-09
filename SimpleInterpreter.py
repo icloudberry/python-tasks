f23fbc1b7fe5b2485e166d9b0e587e239422321f
icloudberry <shikhman.irina@gmail.com>
2018-07-09 08:40:19 +0200

    Simple Interpreter update

--- SimpleInterpreter.py
+++ SimpleInterpreter.py
@@ -1,10 +1,23 @@
+# Write a simple interpreter which understands "+", "-", and "*" operations.
+# Apply the operations in order using command/arg pairs starting with the initial value of `value`.
+# If you encounter an unknown command, return -1.
+#
+#
+# interpret(1, ['+'], [1]) -> 2
+# interpret(4, ['-'], [2]) -> 2
+# interpret(1, ['+', '*'], [1, 3]) -> 6
+
 def interpret(value, commands, args):
     coms = {'+': lambda a,b : a+b, '-': lambda a,b : a-b, '*': lambda a,b : a*b}
 
     for c, a in zip(commands, args):
-        value = coms[c](value, a)
+        if c in coms:
+            value = coms[c](value, a)
+        else:
+            return -1
     return value
 
 print interpret(1, ["+"],[2])
 print interpret(1, ['+', '*','+'], [1, 3, 5])
 print interpret(4, ['-'], [2])
+print interpret(4, ['-',':'], [2, 1])
